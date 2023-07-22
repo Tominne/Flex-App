5d509b083a6bba89e093f5baf8c3addb6dd9532f
Min <min.elliot48@gmail.com>
2023-07-22 12:50:49 +1200

    socket users/rooms setup

--- SparklezChatz/index.js
+++ SparklezChatz/index.js
@@ -23,13 +23,67 @@ app.get('/', function (req, res) {
 
 const CHAT_BOT = 'ChatBot'
 
+let chatRoom = ''
+let allUsers = []
+
 io.on('connection', function (socket) {
   console.log(`User connected ${socket.id}`)
-  socket.on('chat message', function (msg) {
-    io.emit('chat message', msg)
+
+  socket.on('join_room', (data) => {
+    const { username, room } = data
+    socket.join(room)
+
+    let __createdtime__ = Date.now()
+
+    socket.to(room).emit('recieved_message', {
+      message: `${username} has entered the party!`,
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdtime__,
+    })
+    chatRoom = room
+    allUsers.push({ id: socket.id, username, room })
+    chatRoomUsers = allUsers.filter((user) => user.room === room)
+    socket.to(room).emit('chatroom_users', chatRoomUsers)
+
+    harperGetMessages(room)
+      .then((last100Messages) => {
+        socket.emit('last_100_messages', last100Messages)
+      })
+      .catch((err) => console.log('msges not sending/saving'))
   })
-  socket.on('disconnect', function () {
-    console.log('user disconnected')
+  socket.on('send_message', (data) => {
+    const { message, username, room, __createdtime__ } = data
+    io.in(room).emit('recieved messages', data)
+    harperSaveMessage(message, username, room, __createdtime__)
+      .then((response) => console.log(response))
+      .catch((err) => console.log('error responding'))
+  })
+  socket.on('leave_room', (data) => {
+    const { username, room } = data
+    socket.leave(room)
+    const __createdtime__ = Date.now()
+    // Remove user from memory
+    allUsers = leaveRoom(socket.id, allUsers)
+    socket.to(room).emit('chatroom_users', allUsers)
+    socket.to(room).emit('receive_message', {
+      username: CHAT_BOT,
+      message: `${username} has left the chat`,
+      __createdtime__,
+    })
+    console.log(`${username} has left the chat`)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected from the chat')
+    const user = allUsers.find((user) => user.id == socket.id)
+    if (user?.username) {
+      allUsers = leaveRoom(socket.id, allUsers)
+      socket.to(chatRoom).emit('chatroom_users', allUsers)
+      socket.to(chatRoom).emit('receive_message', {
+        message: `${user.username} has disconnected from the party.`,
+      })
+    }
   })
 })
 
