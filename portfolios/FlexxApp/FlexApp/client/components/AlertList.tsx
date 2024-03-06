28c041c94a7391bf83b775ee250a2042712ab2ae
Min <min.elliot48@gmail.com>
2024-03-07 10:33:47 +1300

    db connected

--- portfolios/FlexxApp/FlexApp/client/components/AlertList.tsx
+++ portfolios/FlexxApp/FlexApp/client/components/AlertList.tsx
@@ -54,5 +54,29 @@ export default function AlertList() {
         </section>
       </div>
     )
+  } else {
+    return (
+      <div>
+        There was an error loading any thoughts atm.
+        <br></br>
+        Please come back later
+        <br></br>
+        Cheers...
+        <div>
+          <br></br>
+          <a href="/minstand.jpg">
+            <img className="gif" src="/minstand.jpg" alt="Min Running"></img>
+          </a>
+          <p>This is still me running in a field!</p>
+        </div>
+        <div className="background">
+          <section className="alert-list">
+            <br></br>
+            <h2>Customize a semi coherent thought</h2>
+            <AlertForm />
+          </section>
+        </div>
+      </div>
+    )
   }
 }
